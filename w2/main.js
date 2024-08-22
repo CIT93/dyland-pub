4a6b4eb6f8bf7cc9cccbe963c098b2df75078468
Dylan Donnelly <dylandonnelly@gmail.com>
2024-08-22 13:38:26 -0700

    setup done

---
+++ w2/main.js
@@ -0,0 +1,27 @@
+console.log('Hello World')
+// My Carbon Footprint
+
+// Members of househome
+const myHousehold = 14;
+
+// Size of home
+const myHouse = 4;
+
+// Food choices
+const myFood = 6;
+
+// Water consumption
+const myWater = 8;
+
+// Household purches per year
+const myPurchases = 10;
+
+// Waste produced per week
+const myWaste = 40;
+
+// Amount of waste recycled
+const myRecycling = 18;
+
+// Annual transportation 
+const myDriving = 6;
+

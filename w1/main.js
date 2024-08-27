f949ff86fd812bdcf0977010b7d09b3b60865ea8
Dylan Donnelly <dylandonnelly@gmail.com>
2024-08-26 18:47:45 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+console.log("Hello World")
+
+//let myVar;
+
+const myVar = "dylan";
+const myVarType = typeof(myVar);
+
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+function runNOW() {
+    if (myVarType === "string") {
+        console.log(`line 13`);
+    } else { 
+        console.log(`line 15`);
+    }
+}
+
+runNOW();

374191fa544013335a0e8de147a263ca0e17a1d3
Dylan Donnelly <dylandonnelly@gmail.com>
2024-08-29 23:20:23 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -27,3 +27,39 @@ function determineHouseHoldPts(numberInHouseHold) {
   
   determineHouseHoldPts(3)
   determineHouseHoldPts(4)
+
+
+// Size of Home
+// If you have a large house, then add 10 points to your score.
+// If you have a medium-sized house, then add 7 points.
+// If you have a small house, then add 4 points.
+// If you live in an apartment, then add 2 points.
+
+function determineHouseSizePts(houseSize) {
+    console.log("Inside the function");
+    if (houseSize === "large") {
+      carbonFootprintPoints = carbonFootprintPoints + 10;
+    } else if (houseSize === "medium") {
+      carbonFootprintPoints = carbonFootprintPoints + 7;
+    } else if (houseSize === "small") {
+      carbonFootprintPoints = carbonFootprintPoints + 4;
+    } else if (houseSize === "apartment") {
+      carbonFootprintPoints = carbonFootprintPoints + 2;
+    }
+  
+    console.log(
+      `If house size is ${houseSize} then points will be ${carbonFootprintPoints}.`);
+  }
+  
+  //let carbonFootprintPoints = 0;
+  //const houseSize = "large";
+  
+  
+  
+  // global scale
+  
+  determineHouseSizePts("large")
+  determineHouseSizePts("medium")
+  determineHouseSizePts("small")
+  determineHouseSizePts("apartment")
+
